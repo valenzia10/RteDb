fecffa858cfdf1509e461cf6a379637ea4beaa93
E. Valencia <valenzia10@gmail.com>
2015-08-31 00:17:39 +0200

    Added routing into app.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
 // Angular app
 
-var rteDbApp = angular.module('rteDbApp', []);
+var rteDbApp = angular.module('rteDbApp', ['ngRoute']);
+
+rteDbApp.config(['$routeProvider', function($routeProvider){
+  $routeProvider
+    .when('/', {
+        controller: 'portsController',
+        templateUrl: 'port-list-view.html'
+      })
+    .otherwise({redirectTo: '/'});
+}]);
 
 rteDbApp.controller('portsController', function ($scope) {
   $scope.ports = [{"name": "signalA"},{"name": "signalB"}];
