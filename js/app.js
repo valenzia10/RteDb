430b3758f55cdab438fc95720cd114c140b064f0
E. Valencia <valenzia10@gmail.com>
2015-08-31 00:35:18 +0200

    LocationProvider configured to avoid having to write hashtag in URL.

--- js/app.js
+++ js/app.js
@@ -2,18 +2,21 @@
 
 var rteDbApp = angular.module('rteDbApp', ['ngRoute']);
 
-rteDbApp.config(['$routeProvider', function($routeProvider){
+rteDbApp.config(['$routeProvider', function($routeProvider, $locationProvider){
   $routeProvider
     .when('/', {
         controller: 'portsController',
         templateUrl: 'port-list-view.html'
       })
     .otherwise({redirectTo: '/'});
+    
+    // use the HTML5 History API
+    $locationProvider.html5Mode(true);
 }]);
 
 rteDbApp.controller('portsController', function ($scope) {
   $scope.ports = [{"name": "signalA"},{"name": "signalB"}];
   $scope.clicked = function(){
-    alert('here man!');
+    //window.location.href = window.location.href + 'p';
   };
 });
