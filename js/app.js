b597f38d7dc5ad413540e6caa6a95787da7a9f37
E. Valencia <valenzia10@gmail.com>
2015-08-31 12:56:22 +0200

    Revert "LocationProvider configured to avoid having to write hashtag in URL."
    This reverts commit 430b3758f55cdab438fc95720cd114c140b064f0.

--- js/app.js
+++ js/app.js
@@ -2,21 +2,18 @@
 
 var rteDbApp = angular.module('rteDbApp', ['ngRoute']);
 
-rteDbApp.config(['$routeProvider', function($routeProvider, $locationProvider){
+rteDbApp.config(['$routeProvider', function($routeProvider){
   $routeProvider
     .when('/', {
         controller: 'portsController',
         templateUrl: 'port-list-view.html'
       })
     .otherwise({redirectTo: '/'});
-    
-    // use the HTML5 History API
-    $locationProvider.html5Mode(true);
 }]);
 
 rteDbApp.controller('portsController', function ($scope) {
   $scope.ports = [{"name": "signalA"},{"name": "signalB"}];
   $scope.clicked = function(){
-    //window.location.href = window.location.href + 'p';
+    alert('here man!');
   };
 });
